9f0c5b8cbc06f2f93d06a059c9c65d32d32f2f57
Adam Norman <eltaco@gmail.com>
2017-11-12 21:21:33 +1100

    Righto, figure out why -am isn't working

---
+++ src/app/item.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { Item } from "./models/item";
+import { ITEMS } from "./data/mock-items";
+
+@Injectable()
+export class ItemService {
+
+  constructor() { }
+
+  getItems(): Observable<Item[]> {
+    return of(ITEMS);
+  }
+
+}
