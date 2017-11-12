89b335c2d4de656f1fcecee951e0b4037ac27899
Adam Norman <eltaco@gmail.com>
2017-11-12 16:21:56 +1100

    Actually add everything

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { Store, StoreInventory } from "../models/store";
+import { Item } from "../models/item";
+import { ITEMS } from "../data/mock-items";
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+
+  state = "stock";
+  all_items: Item[] = ITEMS;
+
+  store: Store = {
+    id: "store",
+    name: "Store of the Gods",
+    money: 1000,
+    item_slots: 5,
+    inventory: [
+      { item: ITEMS[0], quantity: 5, price_modifier: 1 },
+      { item: ITEMS[1], quantity: 2, price_modifier: 2 }
+    ]
+  };
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  buyItem(item: Item): void {
+    let inventory: StoreInventory;
+
+    // TODO: how do we do array.select in ES6?
+    for (let inv of this.store.inventory) {
+      if (inv.item === item) {
+        inventory = inv;
+      }
+    }
+
+    if (!inventory) {
+      inventory = { item: item, quantity: 0, price_modifier: 1 };
+      this.store.inventory.push(inventory);
+    }
+
+    inventory.quantity++;
+
+    this.store.money -= item.price;
+  }
+
+  sellItem(inventory: StoreInventory): void {
+    for (let i of this.store.inventory) {
+      if (i === inventory) {
+        i.quantity--;
+        this.store.money += (i.item.price * i.price_modifier);
+
+        if (i.quantity === 0) {
+          let index = this.store.inventory.indexOf(i);
+          this.store.inventory.splice(index, 1);
+        }
+      }
+    }
+  }
+
+  setState(state: string): void {
+    this.state = state;
+  }
+
+}
